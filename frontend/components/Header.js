6cc467d089bddc87b1b4af7c72f1de7f02adcaea
Mehrad Rousta <mehrad77@gmail.com>
2018-12-08 12:28:35 +0330

    import Nav and Meta component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,13 +1,16 @@
+import Nav from './Nav';
+
 const Header = () => (
-    <div>
-      <div className="bar">
-        <a href="">Sick Fits</a>
-      </div>
-      <div className="sub-bar">
-        <p>Search</p>
-      </div>
-      <div>Cart</div>
+  <div>
+    <div className="bar">
+      <a href="">Sick Fits</a>
+      <Nav />
     </div>
-  );
-  
-  export default Header;
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>
+);
+
+export default Header;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Meta from './Meta';
 
 class Page extends Component {
-    render() {
-        return (
-            <div>
-                <Header/>
-                {this.props.children}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
 export default Page;
