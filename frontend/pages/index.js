28fc8c9451bb63ff6791b61ef0245cdfc95c1c9c
Mehrad Rousta <mehrad77@gmail.com>
2018-12-08 12:29:38 +0330

    remove test "Link"

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,9 +3,6 @@ import Link from 'next/link';
 const Home = props => (
     <div>
         <p>Hey!</p>
-        <Link href="/sell">
-            <a>sell</a>
-        </Link>
     </div>
 )
 export default Home;
