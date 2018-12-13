004e6d574711d1169c310d7e6f9cd7589e58124d
Mehrad Rousta <mehrad77@gmail.com>
2018-12-13 22:40:50 +0330

    add updateItem to Mutation resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,27 +1,36 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-      // TODO: Check if they are logged in
-  
-      const item = await ctx.db.mutation.createItem(
-        {
-          data: {
-            ...args,
-          },
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
         },
-        info
-      );
-  
-      console.log(item);
-  
-      return item;
-    },
-    // createDog(parent, args, ctx, info) {
-    //   global.dogs = global.dogs || [];
-    //   // create a dog
-    //   const newDog = { name: args.name };
-    //   global.dogs.push(newDog);
-    //   return newDog;
-    // },
-  };
-  
-  module.exports = Mutations;
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+};
+
+module.exports = Mutations;
