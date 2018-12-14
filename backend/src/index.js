be387358ca5d9eee03145051bf462df0f106b771
Mehrad Rousta <mehrad77@gmail.com>
2018-12-14 11:24:08 +0330

    add user query schema and jwt parser to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,24 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies (JWT)
-// TODO Use express middleware to populate current user
+server.express.use(cookieParser());
+
+// decode JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId into the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
