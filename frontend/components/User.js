f065f9ca7845745cac321b13bda8a3824e61c2e0
Mehrad Rousta <mehrad77@gmail.com>
2018-12-15 18:44:32 +0330

    Show dynamic cart in front-end

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -9,13 +9,24 @@ const CURRENT_USER_QUERY = gql`
       email
       name
       permissions
+      cart {
+        id
+        quantity
+        item {
+          id
+          price
+          image
+          title
+          description
+        }
+      }
     }
   }
 `;
 
 const User = props => (
   <Query {...props} query={CURRENT_USER_QUERY}>
-    {payload => console.log(payload) || props.children(payload)}
+    {payload => props.children(payload)}
   </Query>
 );
 
