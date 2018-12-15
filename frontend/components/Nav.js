a24c78ad3f7bde36359f420e73bcc3fb51d58fbd
Mehrad Rousta <mehrad77@gmail.com>
2018-12-15 16:12:18 +0330

    add static parts of Cart and Local Dat

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
@@ -22,6 +24,11 @@ const Nav = () => (
               <a>{me.name}</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => (
+                <button onClick={toggleCart}>My Cart</button>
+              )}
+            </Mutation>
           </>
         )}
         {!me && (
@@ -36,4 +43,3 @@ const Nav = () => (
 );
 
 export default Nav;
-        
